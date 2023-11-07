39aff3fabbcd69f50d72013948d3f60cdcdfbbd5
Mairaj Pirzada <thisismairaj@gmail.com>
2023-11-07 16:43:08 +0500

    Add find-duplicates route in working condition

---
+++ app/Http/Controllers/DuplicateController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class DuplicateController extends Controller
+{
+    public function findDuplicates(Request $request){
+        $arr = json_decode(array(request()->input('data'))[0]);
+
+        $elementCounts = array();
+        $duplicates = array();
+
+        foreach ($arr as $element) {
+            if (isset($elementCounts[$element])) {
+                $elementCounts[$element]++;
+                if ($elementCounts[$element] === 2) {
+                    $duplicates[] = $element;
+                }
+            } else {
+                $elementCounts[$element] = 1;
+            }
+        }
+
+        return $duplicates;
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AttendanceController;
+use App\Http\Controllers\DuplicateController;
 
 /*
 |--------------------------------------------------------------------------
@@ -26,3 +27,5 @@ Route::post('/upload-attendance-fault', [AttendanceController::class,
 'uploadAttendanceFault']);
 Route::post('/upload-schedule', [AttendanceController::class, 'uploadSchedule']);
 Route::post('/upload-shift', [AttendanceController::class, 'uploadShift']);
+
+Route::post('/find-duplicates', [DuplicateController::class, 'findDuplicates']);
